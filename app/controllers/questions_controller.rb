14758497894d8787459fd03c3c28f3f7c75777d5
millene prado <millene_vprado@hotmail.com>
2021-08-16 14:07:09 -0300

    updated form btn css properties

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,12 +5,12 @@ class QuestionsController < ApplicationController
   def answer
     @user_question = params['question']
 
-    if @user_question == 'I am going to work'
-      @answer = 'Great!'
-    elsif @user_question.end_with?('?')
-      @answer = 'Silly question, get dressed and go to work!'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if @user_question == 'I am going to work'
+                'Great!'
+              elsif @user_question.end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
