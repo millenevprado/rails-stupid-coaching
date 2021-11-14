814b3faaf91a160f6773c66cf32771a97fcd6f7c
millene prado <millene_vprado@hotmail.com>
2021-11-14 18:06:58 -0300

    updated questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,9 @@ class QuestionsController < ApplicationController
   def answer
     @user_question = params['question']
 
-    @answer = if @user_question == 'I am going to work'
+    @answer = if @user_question.blank?
+                "I can't hear you!"
+              elsif @user_question == 'I am going to work'
                 'Great!'
               elsif @user_question.end_with?('?')
                 'Silly question, get dressed and go to work!'
